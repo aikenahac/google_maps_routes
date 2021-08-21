3efc952647e6c7b0a799b357c0f053d587953de2
Aiken Tine Ahac <ahac.aiken@gmail.com>
2021-08-22 00:19:59 +0200

    Provide better documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
 ## [0.0.1] Create package
 
 * Draw routes on google maps with the directions API
+
+## [0.0.2] Provide better documentation
--- lib/google_maps_routes.dart
+++ lib/google_maps_routes.dart
@@ -1,5 +1,6 @@
 library google_maps_routes;
 
+/// Importing necessary packages
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:flutter/material.dart';
@@ -29,6 +30,7 @@ class MapsRoutes {
     late PolylinePoints routePoints = PolylinePoints();
     List<LatLng> routeCoordinates = [];
 
+    /// If the coordinates are null, it returns an empty route
     if (startLat == null ||
         startLon == null ||
         endLat == null ||
@@ -36,34 +38,42 @@ class MapsRoutes {
       return;
     }
 
+    /// If the coordinates are not null, it creates a route between the two points
     PolylineResult result = await routePoints.getRouteBetweenCoordinates(
       googleApiKey,
       PointLatLng(startLat, startLon),
       PointLatLng(endLat, endLon),
     );
 
+    /// Adds coordinates to the route coordinates list
     if (result.points.isNotEmpty) {
       result.points.forEach((PointLatLng point) {
         routeCoordinates.add(LatLng(point.latitude, point.longitude));
       });
     }
 
+    /// Sets a polyline ID
     PolylineId id = PolylineId('route-$routeName');
 
+    /// Creates a polyline with the route coordinates
     Polyline route = Polyline(
         polylineId: id, color: routeColor, points: routeCoordinates, width: 3);
 
+    /// Adds the route to the routes list
     routes.add(route);
   }
 
+  /// Function that creates the actual route between multiple points
   Future<void> drawRoute(List<LatLng> points, String routeName,
       Color routeColor, String googleApiKey) async {
     var previousPoint;
-    print(googleApiKey);
 
+    /// Iterates over the points and creates a route between each pair
     for (var i = 0; i < points.length; i++) {
       var point = points[i];
 
+      /// If the previous point is null it creates a route
+      /// from the first and second point
       if (previousPoint == null) {
         var nextPoint = points[i + 1];
         previousPoint = point;
@@ -75,7 +85,10 @@ class MapsRoutes {
             '${_replaceWhiteSpaces(routeName)}+$i',
             routeColor,
             googleApiKey);
-      } else {
+      }
+      /// If the previous point is not null it creates a route
+      /// between the previous and current point
+      else {
         _createRouteFragment(
             previousPoint.latitude,
             previousPoint.longitude,
@@ -86,8 +99,5 @@ class MapsRoutes {
             googleApiKey);
       }
     }
-
-    print('Routes: ');
-    print(routes);
   }
 }
